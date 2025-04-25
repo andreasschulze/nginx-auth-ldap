dd5fcdbcbe5c4f08683b1a28215806091a76ef9d
A. Schulze <git@andeasschulze.de>
2025-04-25 21:36:01 +0200

    port ist int

--- ngx_http_auth_ldap_module.c
+++ ngx_http_auth_ldap_module.c
@@ -616,7 +616,7 @@ ngx_http_auth_ldap_parse_url(ngx_conf_t *cf, ngx_http_auth_ldap_server_t *server
 
     ngx_conf_log_error(NGX_LOG_NOTICE, cf, 0, "http_auth_ldap: ludpp->lud_scheme=%s", server->ludpp->lud_scheme);
     ngx_conf_log_error(NGX_LOG_NOTICE, cf, 0, "http_auth_ldap: ludpp->lud_host=%s", server->ludpp->lud_host);
-    ngx_conf_log_error(NGX_LOG_NOTICE, cf, 0, "http_auth_ldap: ludpp->lud_port=%s", server->ludpp->lud_port);
+    ngx_conf_log_error(NGX_LOG_NOTICE, cf, 0, "http_auth_ldap: ludpp->lud_port=%i", server->ludpp->lud_port);
     ngx_conf_log_error(NGX_LOG_NOTICE, cf, 0, "http_auth_ldap: ludpp->lud_dn=%s", server->ludpp->lud_dn);
     ngx_memzero(&server->parsed_url, sizeof(ngx_url_t));
     server->parsed_url.url.data = (u_char *) server->ludpp->lud_host;
