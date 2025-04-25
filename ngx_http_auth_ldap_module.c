48fe8301c2f81dfa9a5d7b52a240e261314a0f34
A. Schulze <git@andeasschulze.de>
2025-04-25 21:52:05 +0200

    next

--- ngx_http_auth_ldap_module.c
+++ ngx_http_auth_ldap_module.c
@@ -622,6 +622,11 @@ ngx_http_auth_ldap_parse_url(ngx_conf_t *cf, ngx_http_auth_ldap_server_t *server
     server->parsed_url.url.data = (u_char *) server->ludpp->lud_host;
     server->parsed_url.url.len = ngx_strlen(server->ludpp->lud_host);
     server->parsed_url.default_port = server->ludpp->lud_port;
+    ngx_conf_log_error(NGX_LOG_NOTICE, cf, 0, "http_auth_ldap: parsed_url.url.data=%s", server->parsed_url.url.data);
+    ngx_conf_log_error(NGX_LOG_NOTICE, cf, 0, "http_auth_ldap: parsed_url.default_port=%s", server->parsed_url.default_port);
+    if (ngx_strcmp(server->ludpp->lud_scheme, "ldap") == 0) {
+        return NGX_CONF_OK;
+    }
     if (ngx_parse_url(cf->pool, &server->parsed_url) != NGX_OK) {
         if (server->parsed_url.err) {
             ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "http_auth_ldap: %s in LDAP hostname \"%V\"",
